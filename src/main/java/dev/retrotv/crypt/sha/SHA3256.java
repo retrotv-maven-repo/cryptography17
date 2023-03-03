5bdf8737eb2123943aff33162633ec18aeb52f00
yjj8353 <yjj8353@gmail.com>
2023-03-03 15:01:22 +0900

    SHA3 알고리즘 로직 수정

--- src/main/java/dev/retrotv/crypt/sha/SHA3256.java
+++ src/main/java/dev/retrotv/crypt/sha/SHA3256.java
@@ -2,8 +2,9 @@ package dev.retrotv.crypt.sha;
 
 import dev.retrotv.crypt.Algorithm;
 import dev.retrotv.crypt.OneWayEncryption;
+import dev.retrotv.crypt.owe.Encode;
 
-public class SHA3256 extends SHA3 implements OneWayEncryption {
+public class SHA3256 extends Encode implements OneWayEncryption {
 
     @Override
     public byte[] encrypt(byte[] data) {
