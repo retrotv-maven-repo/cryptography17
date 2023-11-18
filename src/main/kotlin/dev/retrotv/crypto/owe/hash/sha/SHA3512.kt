55a5e36cbafc5eae938641917005b1439bba797e
yjj8353 <yjj8353@gmail.com>
2023-11-18 14:00:24 +0900

    버전업

--- src/main/kotlin/dev/retrotv/crypto/owe/hash/sha/SHA3512.kt
+++ src/main/kotlin/dev/retrotv/crypto/owe/hash/sha/SHA3512.kt
@@ -3,10 +3,10 @@ package dev.retrotv.crypto.owe.hash.sha
 import dev.retrotv.crypto.owe.hash.Hash
 import dev.retrotv.data.utils.binaryToHex
 import dev.retrotv.enums.HashAlgorithm
-import dev.retrotv.utils.MessageDigestEncodeUtil
+import dev.retrotv.utils.encode
 
 class SHA3512 : Hash() {
     override fun hash(data: ByteArray): String {
-        return binaryToHex(MessageDigestEncodeUtil.encode(HashAlgorithm.SHA3512, data))
+        return binaryToHex(encode(HashAlgorithm.SHA3512, data))
     }
 }
