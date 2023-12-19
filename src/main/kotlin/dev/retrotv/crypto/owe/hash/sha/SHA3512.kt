5c1dd84480789eeb65c05246462c70217cdf34a2
yjj8353 <yjj8353@gmail.com>
2023-12-20 00:19:35 +0900

    주석 추가

--- src/main/kotlin/dev/retrotv/crypto/owe/hash/sha/SHA3512.kt
+++ src/main/kotlin/dev/retrotv/crypto/owe/hash/sha/SHA3512.kt
@@ -7,6 +7,12 @@ import dev.retrotv.data.utils.binaryToHex
 import dev.retrotv.enums.Algorithm.Hash.SHA3512
 import dev.retrotv.utils.encode
 
+/**
+ * SHA3-512 알고리즘으로 암호화 하기 위한 [HashAlgorithm] 추상 클래스의 구현체 입니다.
+ *
+ * @author  yjj8353
+ * @since   1.0.0
+ */
 class SHA3512 : HashAlgorithm() {
     override fun hash(data: ByteArray): String {
         return binaryToHex(encode(SHA3512, data))
